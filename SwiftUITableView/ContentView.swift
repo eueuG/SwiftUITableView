bbc2187fb2f9c7775784c10c076c814a238e333b
rinetrous <rinellie1228@icloud.com>
2021-03-23 21:11:42 +0900

    Initial Commit

---
+++ SwiftUITableView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITableView
+//
+//  Created by 野田凜太郎 on 2021/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
